d5d9129554e768fa8f3c596554912cdc9d029308
JOSOA Yassini Jacquerel <yassini@alsasoft.com>
2025-04-05 19:43:01 +0300

    Ajout de l'autoload de Composer dans db.php pour charger les dépendances et amélioration de la gestion des variables d'environnement.

--- db.php
+++ db.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
 // Charger les variables d'environnement
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
@@ -8,7 +11,7 @@ $user = $_ENV['DB_USER'];
 $pass = $_ENV['DB_PASSWORD'];
 $dbname = $_ENV['DB_NAME'];
 $port = $_ENV['DB_PORT'];
-
+ 
 try {
     $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$pass");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
