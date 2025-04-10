fa69ee018d9e3f495ce635523297d8dc834924c7
JOSOA Yassini Jacquerel <yassini@alsasoft.com>
2025-04-11 00:55:34 +0300

    Ajout des en-têtes CORS dans EventCoins.php pour permettre les requêtes cross-origin. Cela améliore la compatibilité avec les API et facilite l'accès aux ressources depuis d'autres domaines.

--- EventCoins.php
+++ EventCoins.php
@@ -1,5 +1,9 @@
-<?php
+<?php 
 
+// CORS
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 include("./db.php");
 
