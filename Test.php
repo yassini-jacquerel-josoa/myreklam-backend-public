916f1a64a3d55ecfc4ff9837ba445c6d4f3ea00f
JOSOA Yassini Jacquerel <yassini@alsasoft.com>
2025-05-12 20:36:00 +0300

    Ajout de l'appel à sendNotificationSubscriptionFree dans Test.php et simplification de la réponse JSON pour indiquer le succès de l'envoi de la notification. Suppression du code commenté pour une meilleure lisibilité.

--- Test.php
+++ Test.php
@@ -8,28 +8,12 @@ $userId = $_POST['userId'];
 
 try {
     $notification = new NotificationBrevoAndWeb($conn);
+    $notification->sendNotificationSubscriptionFree($userId);
  
-    echo json_encode([ 
-        'getTemplateParams' => $notification->getTemplateParams(19),
-        'sendNotificationBrevo' => $notification->sendNotificationBrevo([
-            'email' => 'jooyassini@gmail.com',
-            'templateId' => 19,
-            'params' => [
-                'username' => 'JOOYASSINI',
-            ]
-        ])
+    echo json_encode([
+        'status' => 'success',
+        'message' => 'Notification envoyée avec succès', 
     ]);
-
-    echo "getTemplateParams : 4";
-
-    // $notification->sendNotificationSubscriptionFree($userId);
-
-    // // response
-    // echo json_encode([
-    //     'status' => 'success',
-    //     'message' => 'Notification envoyée avec succès',
-    //     // 'templateId' => $notification->getTemplateId("registration-professional-free")
-    // ]);
 
 } catch (\Throwable $th) {
     echo json_encode([
