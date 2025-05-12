6a92a220dd9f3497d7aa7dd313e5fd5edc00edc3
JOSOA Yassini Jacquerel <yassini@alsasoft.com>
2025-05-12 20:02:42 +0300

    Ajout d'une gestion des exceptions lors de l'envoi de notifications dans Test.php. En cas d'erreur, un message d'erreur est renvoyé au format JSON. Cela améliore la robustesse du code en gérant les erreurs potentielles lors de l'envoi de notifications.

--- Test.php
+++ Test.php
@@ -4,17 +4,19 @@ include_once(__DIR__ . "/packages/NotificationBrevoAndWeb.php");
 
 $userId = $_POST['userId'];
 
-$notification = new NotificationBrevoAndWeb($conn);
-$notification->sendNotificationSubscriptionFree($userId);
+try {
+    $notification = new NotificationBrevoAndWeb($conn);
+    $notification->sendNotificationSubscriptionFree($userId);
 
-// header
-header('Content-Type: application/json');
-
-// response
-echo json_encode([
-    'status' => 'success',
-    'message' => 'Notification envoyée avec succès',
-    'templateId' => $notification->getTemplateId("registration-professional-free")
-]);
-
-?>
+    // response
+    echo json_encode([
+        'status' => 'success',
+        'message' => 'Notification envoyée avec succès',
+        'templateId' => $notification->getTemplateId("registration-professional-free")
+    ]);
+} catch (\Throwable $th) {
+    echo json_encode([
+        'status' => 'error',
+        'message' => $th->getMessage()
+    ]);
+}
