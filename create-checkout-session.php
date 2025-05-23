0e9b04d53ee349505835709da5054d6c2963c098
Giovanni38 <giovannihounsou2@gmail.com>
2025-05-23 10:46:32 +0100

    Public Key change

--- create-checkout-session.php
+++ create-checkout-session.php
@@ -23,7 +23,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 // Stripe SDK
 require_once __DIR__ . '/vendor/autoload.php';
 // \Stripe\Stripe::setApiKey('sk_test_51KEdn8LjQlGQsbAnaApChiGcpV1NcpfX7nJSFzZGBnnlRPqb5P5FlNVCtLhK0uqO26wddWEVNI9KQWpHtOlX9P0g009pMas4Tn');
-\Stripe\Stripe::setApiKey('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');
+// \Stripe\Stripe::setApiKey('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');
+\Stripe\Stripe::setApiKey('pk_live_51KEdn8LjQlGQsbAn8FktZTnAdzff9sURGFoMVhDKOvPrwYqvdO2yD6PTxWg0Vleopg0CIgXq9wYezK5SmQ0mj9qZ00yklzhRrP');
 
 header('Content-Type: application/json');
 
